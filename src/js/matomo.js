de7b6ba2734a5fcdd4f9e6b5383eb1fa8dfb66ec
uppper-productions <99124573+uppper-productions@users.noreply.github.com>
2024-01-28 20:07:57 +0100

    Changed Analytics provider (temporary)

---
+++ src/js/matomo.js
@@ -0,0 +1,10 @@
+  var _paq = window._paq = window._paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://codemodeyt.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='//cdn.matomo.cloud/codemodeyt.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
